7cfce8739b0bcd00e15af8dd53dfa99ce654fecc
divya <gopalakrishnandivya6@gmail.com>
2018-10-30 14:19:15 -0600

    add music and bg

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+$(document).ready(function() {
+
+    // Gets Link for Theme Song
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/sounds/game-of-thrones-theme-song-ringtone-30782.mp3");
+
+    // Theme Button
+    $(".theme-button").on("click", function() {
+      audioElement.play();
+    });
+    $(".pause-button").on("click", function() {
+      audioElement.pause();
+    });
+});
